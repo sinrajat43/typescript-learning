11592a4544b62623bbc6b1ebf0ce06cf215a3dc1
Rajat <sinrajat43@gmail.com>
2022-07-05 16:43:37 +0530

    first

--- index.ts
+++ index.ts
@@ -1,5 +1,26 @@
-var arr = [ 11, 89, 23, 7, 98 ];
-var val = arr.map(Math.log);
-console.log( val );
+// Square root using Map 
+var arr = [16,49,50,225,25,33];
+var val = arr.map(Math.sqrt);
+console.log(val);
+
+// Odd using Filter
+arr = [16,49,50,225,25,33]
+function isOdd(element, index, array) { 
+    return (element%2==0); 
+}      
+var odd=arr.filter(isOdd); 
+console.log(odd);
+
+// Sum using Reduce
+arr = [16,49,50,225,25,33]
+function sum1(a,b) { 
+    return a+b; 
+} 
+           
+var sum=arr.reduce(sum1); 
+console.log(sum);
+
+
+
 
 
